57f539e60d2c051891f130619807beeeac5f5ec0
hhchaudh <haaris.h.chaudhry@gmail.com>
2016-11-26 06:07:09 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { LobbyComponent } from './components/lobby/lobby.component';
+import { GameComponent } from './components/game/game.component';
+import { NewUserFormComponent } from './components/new-user-form/new-user-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LobbyComponent,
+    GameComponent,
+    NewUserFormComponent
   ],
   imports: [
     BrowserModule,
