f48b0f30ff310eabd27408f3f8e7d81ca01cfc10
hhchaudh <haaris.h.chaudhry@gmail.com>
2016-11-26 10:03:47 -0600

    added the game which can be accessed from the lobby by clicking the "join game" button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import {RouterModule, Routes} from "@angular/router";
 import {NewUserFormComponent} from "./components/new-user-form/new-user-form.component";
 import {LoginComponent} from "./components/login/login.component";
 import {LobbyComponent} from "./components/lobby/lobby.component";
+import {GameComponent} from "./components/game/game.component";
 
 const APP_ROUTES: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'NewUser', component: NewUserFormComponent},
   {path: 'login', component: LoginComponent},
   {path: 'lobby', component: LobbyComponent},
+  {path: 'game', component: GameComponent},
   {path: '**', redirectTo: 'login', pathMatch: 'full'}
 ];
 
